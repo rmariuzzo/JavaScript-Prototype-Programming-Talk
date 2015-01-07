a1af9575a21bd0b7665fcf5e9368f92451db592b
Rubens Mariuzzo <rubens@mariuzzo.com>
2015-01-07 13:13:27 -0400

    Test added to test static properties.

--- spec/04-object-properties.spec.js
+++ spec/04-object-properties.spec.js
@@ -35,4 +35,35 @@ describe('Objects', function () {
     expect(obj3).to.have.ownProperty('prop3');
   });
 
+  it('can have static properties', function() {
+    var obj1 = {};
+    var obj2 = Object.create(obj1);
+    var obj3 = Object.create(obj1);
+
+    // Adding a property to a prototype object will make all descendant object
+    // to have access to it.
+    obj1.prop1 = 'foo';
+    expect(obj2).to.have.property('prop1');
+    expect(obj2).to.not.have.ownProperty('prop1');
+    expect(obj3).to.have.property('prop1');
+    expect(obj3).to.not.have.ownProperty('prop1');
+
+    // Changing the value of the static property directly in the prototype
+    // will make all descendant have a the same value.
+    var newValue = 'bar';
+    obj1.prop1 = newValue;
+    expect(obj2).to.have.property('prop1', newValue);
+    expect(obj3).to.have.property('prop1', newValue);
+
+    // Trying to change the value of the static property in a descendant object,
+    // not the prototype, will result in shadowing, creating a new instance
+    // property with a new value. So far we can conclude that instance properties
+    // have higher priority in the prototype chain than static properties.
+    newValue = 'xyz';
+    obj2.prop1 = newValue;
+    expect(obj2).to.have.property('prop1', newValue);
+    expect(obj1).to.not.have.property('prop1', newValue);
+    expect(obj3).to.not.have.property('prop1', newValue);
+  });
+
 });
